17845477166e9f2f9efeba4c65c50ce9a8a58af2
Rixbroho <ratnaprajapati10@gmail.com>
2026-01-22 01:43:51 +0545

    using jwt

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express= require('express');
+const cookieParser=require('cookie-parser');
 const app=express();
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 const port=3000;
 
+app.use(cookieParser());
+
+
 app.get('/',(req,res)=>{
-    res.cookie("name","Ram")
+    let token=jwt.sign({email:"try@gmail.com"},"secret");
+    console.log(token)
+    res.cookie('token',token);
     res.send("hello world")
 });
 
-app.get('/app',(req,res)=>{
-    res.send("app");
-});
 
 app.listen(port,()=>{
     console.log(`backend is working in ${port} port`)
