16e40baf88a1aec5a312ea5d09407cd952166a2b
Rixbroho <ratnaprajapati10@gmail.com>
2026-01-23 23:25:51 +0545

    adding commend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port=3000;
 
 app.use(cookieParser());
 
+//makeing token and storing in cookie
 app.get('/',(req,res)=>{
     let token=jwt.sign({email:"ok@gmail.com"},"secret");
     // console.log(token)
@@ -14,12 +15,14 @@ app.get('/',(req,res)=>{
     res.send("hello world")
 });
 
+//verifying token from cookie
 app.get('/app',(req,res)=>{
     let data=jwt.verify(req.cookies.token,'secret')
     console.log(data);
     res.send("done")
 })
 
+// listening to server
 app.listen(port,()=>{
     console.log(`backend is working in ${port} port`)
 });
